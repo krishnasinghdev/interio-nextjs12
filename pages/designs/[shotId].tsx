0fce08ceda1d4b1037ddb96674f291a83810fc76
Krishna <singhks0054@gmail.com>
2023-05-10 17:52:38 +0530

    error in shotid

--- pages/designs/[shotId].tsx
+++ pages/designs/[shotId].tsx
@@ -83,163 +83,168 @@ const ShotId = ({
 
   return (
     <SideLayout>
-      {/* @ts-ignore */}
-      {isLogin && (
-        <>
-          <header className='flex justify-between '>
-            <div className='flex gap-x-4'>
-              <Image
-                src={'/dp.png'}
-                height={40}
-                width={40}
-                alt='man dp'
-                className='rounded-full bg-primary '
-              />
-              <div>
-                <h1>{vendor.vendor}</h1>
-                <p className='text-xs text-gray'>21 shots uploaded till now</p>
+      <>
+        {isLogin && (
+          <>
+            <header className='flex justify-between '>
+              <div className='flex gap-x-4'>
+                <Image
+                  src={'/dp.png'}
+                  height={40}
+                  width={40}
+                  alt='man dp'
+                  className='rounded-full bg-primary '
+                />
+                <div>
+                  <h1>{vendor.vendor}</h1>
+                  <p className='text-xs text-gray'>
+                    21 shots uploaded till now
+                  </p>
+                </div>
               </div>
+              <Link
+                href={'/designs/upload'}
+                className='rounded bg-primary px-4 py-2'
+              >
+                Upload Shot
+              </Link>
+            </header>
+            <p className='my-8 w-full border-[0.5px] border-gray' />
+          </>
+        )}
+        <div className='mb-8 flex justify-between'>
+          <div className='flex gap-x-4'>
+            <Image
+              src={'/girl.png'}
+              height={40}
+              width={40}
+              alt='man dp'
+              className='rounded-lg bg-primary '
+            />
+            <div>
+              <h1>{title}</h1>
+              <p className='text-xs text-gray'>{owner.name}</p>
             </div>
-            <Link
-              href={'/designs/upload'}
-              className='rounded bg-primary px-4 py-2'
+          </div>
+          <div className='flex gap-x-4 '>
+            <button
+              className='cborder rounded bg-trans px-4 py-2'
+              onClick={saveHandler}
             >
-              Upload Shot
-            </Link>
-          </header>
-          <p className='my-8 w-full border-[0.5px] border-gray' />
-        </>
-      )}
-      <div className='mb-8 flex justify-between'>
-        <div className='flex gap-x-4'>
-          <Image
-            src={'/girl.png'}
-            height={40}
-            width={40}
-            alt='man dp'
-            className='rounded-lg bg-primary '
-          />
-          <div>
-            <h1>{title}</h1>
-            <p className='text-xs text-gray'>{owner.name}</p>
+              Save
+            </button>
+            <button
+              className='rounded border border-pink-500 bg-trans px-4 py-2'
+              onClick={likeHandler}
+            >
+              <Image
+                src={'/pheart.png'}
+                alt='heart-icon'
+                height={20}
+                width={20}
+                className='inline'
+              />{' '}
+              Like
+            </button>
           </div>
         </div>
-        <div className='flex gap-x-4 '>
-          <button
-            className='cborder rounded bg-trans px-4 py-2'
-            onClick={saveHandler}
-          >
-            Save
-          </button>
-          <button
-            className='rounded border border-pink-500 bg-trans px-4 py-2'
-            onClick={likeHandler}
-          >
-            <Image
-              src={'/pheart.png'}
-              alt='heart-icon'
-              height={20}
-              width={20}
-              className='inline'
-            />{' '}
-            Like
-          </button>
-        </div>
-      </div>
-      {/* @ts-ignore */}
-      <Image src={images[0].url} alt='bed' height={500} width={1400} />
-      <div className='my-8 flex items-center justify-center gap-4'>
-        <button className='cborder rounded bg-trans px-4 py-2'>
-          <BsChatDots />
-        </button>
-        <button className='cborder rounded bg-trans px-4 py-2'>
-          <FiFolderMinus />
-        </button>
-        <button className='cborder rounded bg-trans px-4 py-2'>
-          <BsShareFill />
-        </button>
-      </div>
-      <p className='my-8 w-full border-[0.5px] border-gray' />
-
-      <p>{description}</p>
-
-      <h2>I am available for new projects</h2>
-      <p className='my-2'>
-        📪 Email:
-        <span className='text-primary'> {owner.email}</span>
-      </p>
-      <p className='mb-2'>
-        🎯 Linkedin: <span className='text-primary'>Raj_Singh 😀</span>
-      </p>
-      <p className='mb-2'>
-        👋 Instagram: <span className='text-primary'> @uxonfire</span>
-      </p>
-
-      {owner._id === vendor._id && (
+        {/* @ts-ignore */}
+        <Image src={images[0].url} alt='bed' height={500} width={1400} />
         <div className='my-8 flex items-center justify-center gap-4'>
-          <button className='cborder rounded bg-trans px-4 py-2'>Edit</button>
           <button className='cborder rounded bg-trans px-4 py-2'>
-            Edit Details
+            <BsChatDots />
           </button>
-          <button className='cborder rounded bg-trans px-4 py-2'>Delete</button>
+          <button className='cborder rounded bg-trans px-4 py-2'>
+            <FiFolderMinus />
+          </button>
+          <button className='cborder rounded bg-trans px-4 py-2'>
+            <BsShareFill />
+          </button>
         </div>
-      )}
-      <div className='flex flex-col items-center justify-items-center gap-y-2 '>
-        <div className='grid  grid-cols-12 '>
-          <p className='col-span-5 my-8 w-full border-[0.5px] border-gray' />
-          <Image
-            src={'/girl.png'}
-            height={40}
-            width={40}
-            alt='man dp'
-            className='col-span-2 mx-4 rounded-full bg-primary'
-          />
-          <p className='col-span-5 my-8 w-full border-[0.5px] border-gray' />
-        </div>
-        <h1>{owner.name}</h1>
-        <p>Lorem ipsum dolor sit amet consectetur.</p>
-        <button onClick={makeChat} className='rounded bg-primary px-4 py-2'>
-          {message ? message : "Let's chat"}
-        </button>
-      </div>
+        <p className='my-8 w-full border-[0.5px] border-gray' />
 
-      <div className='flex flex-col gap-y-8 mt-4'>
-        <div className='flex justify-between'>
-          <p>More from {owner.name}</p>
-          <p className='cursor-pointer text-primary hover:underline'>
-            View Profile
-          </p>
+        <p>{description}</p>
+
+        <h2>I am available for new projects</h2>
+        <p className='my-2'>
+          📪 Email:
+          <span className='text-primary'> {owner.email}</span>
+        </p>
+        <p className='mb-2'>
+          🎯 Linkedin: <span className='text-primary'>Raj_Singh 😀</span>
+        </p>
+        <p className='mb-2'>
+          👋 Instagram: <span className='text-primary'> @uxonfire</span>
+        </p>
+
+        {owner._id === vendor._id && (
+          <div className='my-8 flex items-center justify-center gap-4'>
+            <button className='cborder rounded bg-trans px-4 py-2'>Edit</button>
+            <button className='cborder rounded bg-trans px-4 py-2'>
+              Edit Details
+            </button>
+            <button className='cborder rounded bg-trans px-4 py-2'>
+              Delete
+            </button>
+          </div>
+        )}
+        <div className='flex flex-col items-center justify-items-center gap-y-2 '>
+          <div className='grid  grid-cols-12 '>
+            <p className='col-span-5 my-8 w-full border-[0.5px] border-gray' />
+            <Image
+              src={'/girl.png'}
+              height={40}
+              width={40}
+              alt='man dp'
+              className='col-span-2 mx-4 rounded-full bg-primary'
+            />
+            <p className='col-span-5 my-8 w-full border-[0.5px] border-gray' />
+          </div>
+          <h1>{owner.name}</h1>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <button onClick={makeChat} className='rounded bg-primary px-4 py-2'>
+            {message ? message : "Let's chat"}
+          </button>
         </div>
-        <div className='flex gap-4 overflow-x-auto '>
-          {[1, 2, 3, 4].map((_, i) => (
-            <div key={i} className='mb-4'>
-              <Link href='/designs/single'>
-                <Image
-                  src={`/l${i + 1}.png`}
-                  alt='l1img'
-                  height={250}
-                  quality={100}
-                  width={270}
-                  className='cursor-pointer transition-all hover:scale-105 '
-                />
-              </Link>
-              <div className='flex justify-between px-4 py-2 text-gray'>
-                <span>
-                  <BsChatDots className='inline' /> 1.1k
-                </span>
-                <p className='flex gap-4'>
+
+        <div className='mt-4 flex flex-col gap-y-8'>
+          <div className='flex justify-between'>
+            <p>More from {owner.name}</p>
+            <p className='cursor-pointer text-primary hover:underline'>
+              View Profile
+            </p>
+          </div>
+          <div className='flex gap-4 overflow-x-auto '>
+            {[1, 2, 3, 4].map((_, i) => (
+              <div key={i} className='mb-4'>
+                <Link href='/designs/single'>
+                  <Image
+                    src={`/l${i + 1}.png`}
+                    alt='l1img'
+                    height={250}
+                    quality={100}
+                    width={270}
+                    className='cursor-pointer transition-all hover:scale-105 '
+                  />
+                </Link>
+                <div className='flex justify-between px-4 py-2 text-gray'>
                   <span>
-                    <AiOutlineHeart className='inline' /> 1.1k
+                    <BsChatDots className='inline' /> 1.1k
                   </span>
-                  <span>
-                    <AiOutlineEye className='inline' /> 1.1k
-                  </span>
-                </p>
+                  <p className='flex gap-4'>
+                    <span>
+                      <AiOutlineHeart className='inline' /> 1.1k
+                    </span>
+                    <span>
+                      <AiOutlineEye className='inline' /> 1.1k
+                    </span>
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      </>
     </SideLayout>
   );
 };
